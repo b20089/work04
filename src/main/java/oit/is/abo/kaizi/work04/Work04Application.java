d060e99c378257c9d69db09091776f23ec4ae9af
b20089 <e1b20089@oit.ac.jp>
2022-10-18 14:07:12 +0900

    初期化

---
+++ src/main/java/oit/is/abo/kaizi/work04/Work04Application.java
@@ -0,0 +1,13 @@
+package oit.is.abo.kaizi.work04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Work04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Work04Application.class, args);
+	}
+
+}
